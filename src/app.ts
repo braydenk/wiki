ca2b1ee1089740661409de4593c7a51bb83e85d8
Brayden Killeen <braydenmkilleen@gmail.com>
2020-05-20 23:11:58 +1000

    Setup db connection

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,25 @@
 import express from 'express';
+import mongoose, { Schema } from 'mongoose';
+
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind('console', 'connection error:'))
+db.once('open', () => 'connected to db')
+
+
+const userSchema: Schema = new Schema({
+  name: String
+})
+
+interface UserSchema extends Document {
+  name: string
+}
+const User = mongoose.model<UserSchema>('User', userSchema)
+
+
+const user = new User('john')
+console.log(user)
 
 
 const app = express();
